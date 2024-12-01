14cb0c4e55df0ba68803e75c9644684c67a8b831
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2024-12-01 13:54:42 -0300

    Cenas e Nearest pras imagens

--- gameJam2024.2/scripts/global.gd
+++ gameJam2024.2/scripts/global.gd
@@ -3,3 +3,12 @@ extends Node
 var player_anda = true
 var maldicao = true
 var pistas_do_player = []
+var pegou_peruca = false
+var pegou_capa = false
+
+func _process(delta: float) -> void:
+	sairdojogo()
+
+func sairdojogo() -> void:
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
--- gameJam2024.2/scripts/world.gd
+++ gameJam2024.2/scripts/world.gd
@@ -3,13 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    
-    Global.player_anda = false
-    #Dialogic.start("Inicio")
-    
+	Global.player_anda = false
+	Dialogic.start("Inicio")
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if Dialogic.timeline_ended:
-        Global.player_anda = true
-    pass
+	if Dialogic.timeline_ended:
+		Global.player_anda = true
