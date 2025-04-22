2a1a9d6d78c5a332113210cbe461c377e925917f
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-04-21 21:00:44 -0300

    feat: camera seguindo jogador

--- gameJam2024.2/scripts/vila.gd
+++ gameJam2024.2/scripts/vila.gd
@@ -9,3 +9,9 @@ func _on_level_spawn(destination_tag: String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
