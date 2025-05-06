e07011a08e3ef3a7fc3939732d98f4b15c3887ea
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2025-05-06 13:56:09 -0300

    Oca, Vila e transição

--- gameJam2024.2/scripts/transição.gd
+++ gameJam2024.2/scripts/transição.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    $AnimationPlayer.play("Fade in")
+	$AnimationPlayer.play("Fade in")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    pass
+	pass
