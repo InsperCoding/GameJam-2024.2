bb9880a58912d7ca024f4c37a460e2682025b842
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-13 15:02:27 -0300

    feat: overlay para diario

--- gameJam2024.2/scripts/NPC_peruca.gd
+++ gameJam2024.2/scripts/NPC_peruca.gd
@@ -4,25 +4,25 @@ var player_in_range = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if player_in_range and Input.is_action_just_pressed("interact"):
-        Global.pegou_peruca = true
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		Global.pegou_peruca = true
 
 
 func _on_area_2d_area_entered(body: CharacterBody2D) -> void:
-    if body.is_in_group("player"):
-        player_in_range = true
-        print("NPC.player_in_range = " + str(player_in_range))
-        
-        if Input.is_action_just_pressed("interact"):
-            Dialogic.start("peruca")
-        
+	if body.is_in_group("player"):
+		player_in_range = true
+		print("NPC.player_in_range = " + str(player_in_range))
+		
+		if Input.is_action_just_pressed("interact"):
+			Dialogic.start("peruca")
+		
 
 
 func _on_area_2d_area_exited(body: CharacterBody2D) -> void:
-    player_in_range = false
-    print("NPC.player_in_range = " + str(player_in_range))
+	player_in_range = false
+	print("NPC.player_in_range = " + str(player_in_range))
--- gameJam2024.2/scripts/player.gd
+++ gameJam2024.2/scripts/player.gd
@@ -15,6 +15,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Global.player_anda:
 		player_movement(delta)
+		
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("open_diary"):
+		$"../Diario".visible = !$"../Diario".visible
 
 func player_movement(delta):
 	if Input.is_action_pressed("right"):
