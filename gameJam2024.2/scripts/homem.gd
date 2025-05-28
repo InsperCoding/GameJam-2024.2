f01a8a8698c6caac0a0d1c997ce5990e0ebcaf98
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-28 10:36:27 -0300

    feat: interação de teste

--- gameJam2024.2/scripts/homem.gd
+++ gameJam2024.2/scripts/homem.gd
@@ -1,5 +1,24 @@
 extends Node
 
+var player_in_range = false
+var ja_interagiu = false
+
+func interacao() -> void:
+	if !ja_interagiu:
+		ja_interagiu = true
+		# FALTA AQUI:
+		# Habilitar label baseada no numero aprendido, colocando texto adequando
+		Global.num_palavras_novas += 1
+		
+		for palavra in get_meta("novas_palavras"):
+			print(palavra)
+			Dialogic.VAR.set_variable(palavra, palavra)
+			var label = $"../Diario".get_children()[0].get_children()[Global.num_palavras_novas]
+			label.show()
+			label.text = palavra
+			
+	#Dialogic.start("conversa_homem1")
+	Dialogic.start(get_meta("dialogo"))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +27,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		interacao()
+
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+		
+		if Input.is_action_just_pressed("interact"):
+			interacao()
+			
+func _on_area_2d_area_exited(body: CharacterBody2D) -> void:
+	player_in_range = false
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	Dialogic.start("conversa_homem1")
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_in_range = false
