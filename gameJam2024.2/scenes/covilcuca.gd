4fc219dcb7809a79ba2a49ae5f560f05437ba320
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-05-30 12:14:04 -0300

    feat: cenas melhoradas

--- gameJam2024.2/scenes/covilcuca.gd
+++ gameJam2024.2/scenes/covilcuca.gd
@@ -4,8 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.start("com_a_cuca")
+	if NavigationManager.spawn_door_tag != null:
+		_on_level_spawn(NavigationManager.spawn_door_tag)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_level_spawn(destination_tag: String):
+	var door_path = "Doors/Door_" + destination_tag
+	var door = get_node(door_path) as Door
+	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
