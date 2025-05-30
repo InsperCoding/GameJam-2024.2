bcbcf839f2397d8bcb3539425f60861f35190af1
rafaelgp3 <rafaelgp3@al.insper.edu.br>
2025-05-30 15:15:35 -0300

    feat: hotbar funcional

--- gameJam2024.2/scripts/global.gd
+++ gameJam2024.2/scripts/global.gd
@@ -15,9 +15,19 @@ var num_palavras_novas = 0
 var palavras_ouvidas = []
 var traducoes_selecionadas = []
 
+# Variaveis relacionadas ao inventario
+var inventario = []
+
 func _process(_delta: float) -> void:
 	sairdojogo()
 
 func sairdojogo() -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+		
+func coletar_item(item: String) -> void:
+	var hotbar = get_tree().get_current_scene().get_child(-1)
+	var espaco_no_inventario = hotbar.get_child(0).get_child(0).get_child(2*inventario.size())
+	print(espaco_no_inventario)
+	espaco_no_inventario.texture = load(item)
+	inventario.append(item)
