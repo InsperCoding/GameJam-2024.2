1ad3c509486ce50e38fd65b62d43cd824a86168f
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-05-29 23:25:34 -0300

    feat: transicao helicoptero

--- gameJam2024.2/scripts/Escritorio.gd
+++ gameJam2024.2/scripts/Escritorio.gd
@@ -1,13 +1,11 @@
 extends Node
+@onready var helicoptero_som_player: AudioStreamPlayer = $HelicopteroSom 
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.start("Inicio")
 	Dialogic.timeline_ended.connect(_on_timeline_finished)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
@@ -16,4 +14,14 @@ func _on_timeline_finished():
 	$Timer.start()
 
 func _on_timer_timeout() -> void:
+	var imagem_node: TextureRect = get_node("TransicaoHeli")
+	
+	imagem_node.visible = true
+	helicoptero_som_player.play() 
+	
+	await get_tree().create_timer(2.0).timeout
+	
+	imagem_node.visible = false 
+	helicoptero_som_player.stop()
+	
 	get_tree().change_scene_to_file("res://scenes/Vila.tscn")
